0766f87cbb2dafe23b57e251609fefc3ca085a99
Brando Vidal <yisusdracon@gmail.com>
2022-12-09 06:32:58 -0500

    ⚒  use Style Module in Layout
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,12 @@
+import styles from './index.module.css'
+
 import { Props } from '../interfaces/Children'
 
 import { Footer } from './Footer'
 import { Header } from './Header'
 
 export const Layout = ({ children = '' }: Props): React.ReactElement => (
-  <div className='container'>
+  <div className={styles.container}>
     <Header text='Buttons' />
     {children}
     <Footer text='created by @brandovidal - devChallenges.io' />
