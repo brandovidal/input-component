1c0747a7ba05d74b6af7fbe3e9dce68e0b547f60
Brando Vidal <yisusdracon@gmail.com>
2022-12-25 17:08:11 -0500

    ⚒  configure vitest
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,43 +1,7 @@
-import { afterAll, beforeAll, describe, test } from 'vitest'
-import { preview } from 'vite'
-import type { PreviewServer } from 'vite'
+import { describe, expect, test } from 'vitest'
 
-import { chromium } from 'playwright'
-import type { Browser, Page } from 'playwright'
-import { expect } from '@playwright/test'
-
-describe('App', async () => {
-  let server: PreviewServer
-  let browser: Browser
-  let page: Page
-
-  beforeAll(async () => {
-    server = await preview({ preview: { port: 5173 } })
-    browser = await chromium.launch()
-    page = await browser.newPage()
+describe('App', () => {
+  test('should add two numbers', () => {
+    expect(1 + 1).toBe(2)
   })
-
-  afterAll(async () => {
-    await browser.close()
-    await new Promise<void>((resolve, reject) => {
-      server.httpServer.close((error) => (error != null ? reject(error) : resolve()))
-    })
-  })
-
-  test('should have to be defined button', async () => {
-    await page.goto('http://localhost:5173')
-
-    const button = page.locator('button[class*="md"]').first()
-
-    expect(button).toBeDefined()
-  }, 3_000)
-
-  test('should have default button', async () => {
-    await page.goto('http://localhost:5173')
-
-    const button = page.locator('button[class*="md"]').first()
-    expect(button).toBeDefined()
-
-    await expect(button).toHaveText('Default')
-  }, 3_000)
 })
